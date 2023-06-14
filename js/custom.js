0fd2179da974f0a14ceb7414b97c7810c3b46f2f
anildalar <dollorinfotech@gmail.com>
2023-06-14 09:11:41 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+//Syntax
+//var variablename=variableValue;
+
+// Create a function
+//1.Function Defination/Name define
+//Function syntax
+/*
+function <functionName>(){
+    //Function body
+} 
+
+*/
+
+//1. Function defination is a one time process
+function greetMyStudents(name){ //I will recive the formal argument
+    console.log("Hello "+name);
+}
+
+//2. Invoke/Call   Invoking/Calling
+// Function calling is a many time process
+greetMyStudents("Anil");// We can pass some actual arguments
+greetMyStudents('Goutam');
+greetMyStudents(`Abhishek`);
+
+
+
+
+
+//
+
+//2. Function Calling/Invocation  Call/Invoke
+
+
+
+// Create a variableName
+
---
+++ js/custom3.js
@@ -0,0 +1,17 @@
+
+
+//student 2
+
+// I will define the function name with camleCase
+//1. Function defination is a one time process
+function addTwoNumbers(n1,n2){
+    console.log(n1-5);
+    //Function body
+}
+
+//2. Function invoking/calling is a many time process
+
+addTwoNumbers(10,20);
+addTwoNumbers(50,20);
+
+
